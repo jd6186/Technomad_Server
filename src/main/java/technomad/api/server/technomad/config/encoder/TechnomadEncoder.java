7eccd8370680c03c7cb3731a2a8c958f4d431374
jeongdonguk <wjdehd7991@gmail.com>
2023-05-24 13:34:29 +0900

    공통 작업 1. Swagger UI 적용 내용 변경 2. Encoder 생성

---
+++ src/main/java/technomad/api/server/technomad/config/encoder/TechnomadEncoder.java
@@ -0,0 +1,12 @@
+package technomad.api.server.technomad.config.encoder;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+public class TechnomadEncoder {
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+}
--- src/main/java/technomad/api/server/technomad/config/web/SecurityConfig.java
+++ src/main/java/technomad/api/server/technomad/config/web/SecurityConfig.java
@@ -1,8 +1,7 @@
 package technomad.api.server.technomad.config.web;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -12,42 +11,30 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import technomad.api.server.technomad.core.auth.JwtAuthProvider;
 import technomad.api.server.technomad.core.filter.JwtAuthenticationFilter;
 
+@Configuration
 @EnableWebSecurity
-@Slf4j
 public class SecurityConfig {
     private final JwtAuthProvider jwtAuthProvider;
     private final CorsConfigurationSource corsConfigurationSource;
-
     public SecurityConfig(JwtAuthProvider jwtAuthProvider, CorsConfigurationSource corsConfigurationSource){
         this.jwtAuthProvider = jwtAuthProvider;
         this.corsConfigurationSource = corsConfigurationSource;
     }
 
-    /**
-     * Security Filter Chain을 통해 인증 관리
-     * Security 적용 Tip
-     * antMatchers 작성 시 permitAll 하고 싶은 목록이 차단하는 URL 경로보다 먼저 작성되어야 정상적으로 permitAll을 적용 가능
-     * Security를 활용해 비허가 요청을 정상적으로 차단하기 위해선 .anyRequest().authenticated() 를 반드시 작성해 기능을 작동시켜야 함
-     * @param http - Spring이 관리하는 곳으로 진입하는 모든 Http 요청
-     * @throws Exception - 예외
-     */
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        return http.headers().disable() // Base64 File 전송
-                .httpBasic().disable()
-                .formLogin().disable()
-                .csrf().disable()
-                .cors().configurationSource(corsConfigurationSource)
-                .and().sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
-                .and().authorizeRequests()
-                // TODO - 개발 완료 시 주석 해제하고 아래 /** > permitAll은 제거 > 추후 메뉴별로 anyMatchers 만들어야 함
-                .requestMatchers("/**").permitAll()
+        http.headers().disable().csrf().disable().cors().configurationSource(corsConfigurationSource);
+        http.httpBasic().disable().formLogin().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        // TODO - 개발 완료 시 주석 해제하고 아래 /** > permitAll은 제거 > 추후 메뉴별로 anyMatchers 만들어야 함
+        http.authorizeRequests()
+            .requestMatchers("/**").permitAll()
 //                .antMatchers("/v3/api-docs/**", "/api-docs/**", "/swagger-ui.html", "/swagger-ui/**", "/guest/**").permitAll()
 //                .antMatchers("/**").hasAnyRole("MASTER", "NORMAL_MANAGER")
-                .anyRequest().authenticated()
-                .and()
-                .addFilterBefore(new JwtAuthenticationFilter(jwtAuthProvider), UsernamePasswordAuthenticationFilter.class)
-                .build();
+            .anyRequest().authenticated()
+            .and()
+            .addFilterBefore(new JwtAuthenticationFilter(jwtAuthProvider), UsernamePasswordAuthenticationFilter.class);
+        return http.build();
     }
 }
